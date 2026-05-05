7fbd678b6c68f6149b17970d1c882b4004b6a65a
Serein429 <2794708664@qq.com>
2026-05-05 14:53:38 +0800

    Update embed.py

--- app/rag/embed.py
+++ app/rag/embed.py
@@ -1,9 +1,6 @@
 from sentence_transformers import SentenceTransformer
 
-
-# ✅ 多语言模型（1024维）
-model_path = r"D:\models\bge-m3\BAAI\bge-m3"
-model = SentenceTransformer(model_path)
+model = SentenceTransformer("BAAI/bge-m3")
 
 def embed(texts):
     return model.encode(texts, normalize_embeddings=True).tolist()
